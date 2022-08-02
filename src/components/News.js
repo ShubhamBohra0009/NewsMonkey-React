19fa229de57e422f88d23cde16a45b3921210431
Shubham_Bohra <shubhambohra0009@gmail.com>
2022-08-02 22:49:02 +0530

    tuto 28 completed

--- src/components/News.js
+++ src/components/News.js
@@ -40,23 +40,34 @@ export class News extends Component {
         // console.log("hi");
         this.state= {
             articles: this.articles,
+            // articles: [],
             loading: false
         }
     }
+
+    async componentDidMount(){
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=4a92330d3bc64b9e8db888ea42edb5b8";
+        let data= await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({articles: parsedData.articles})
+    }
+
   render() {
     return (
       <div className='container my-3'>
-        <h2>NewsMonkey - Top Headlines</h2>
+        <h1>NewsMonkey - Top Headlines</h1>
 
         <div className="row">
         {this.state.articles.map((element)=>{
             
         return  <div className="col-md-4 " key={element.url}>
-            <NewsItem title={element.title.slice(0,45)} description={element.description.slice(0,88)}  newsUrl={element.url} imageUrl={element.urlToImage}/>
+            <NewsItem title={element.title?element.title :""} description={element.title?element.description :""}  newsUrl={element.url} imageUrl={element.urlToImage}/>
+            {/* <NewsItem title={element.title?element.title.slice(0,45) :""} description={element.title?element.description.slice(0,88) :""}  newsUrl={element.url} imageUrl={element.urlToImage}/> */}
             </div>
         })}
         </div>
-      </div>
+      </div> 
     )
   }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -4,13 +4,14 @@ export class NewsItem extends Component {
   render() {
     let {title, description,imageUrl,newsUrl}= this.props;
     return (
-      <div>
+      <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-        <img src={imageUrl} className="card-img-top" alt="..."/>
+        {/* <img src={!imageUrl? "https://res.cloudinary.com/teepublic/image/private/s--79EwJk3z--/t_Preview/b_rgb:000000,c_limit,f_auto,h_630,q_90,w_630/v1608236443/production/designs/17519845_0.jpg" :imageUrl} className="card-img-top" style={{maxHeight:"160.88px"}} alt="..."/> */}
+        <img src={!imageUrl? "https://res.cloudinary.com/teepublic/image/private/s--79EwJk3z--/t_Preview/b_rgb:000000,c_limit,f_auto,h_630,q_90,w_630/v1608236443/production/designs/17519845_0.jpg" :imageUrl} className="card-img-top"  alt="..."/>
         <div className="card-body ">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+            <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary mx-auto">Read More</a>
         </div>
         </div>
       </div>
